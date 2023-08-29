d0b29698de8723a7c38895b6d4970bffa82c6fb5
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-29 12:26:24 -0500

    Updated Pokemon

--- wb-react-2/src/Pokemon.jsx
+++ wb-react-2/src/Pokemon.jsx
@@ -6,13 +6,25 @@ const Pokemon = (props) => {
   const [pokemon, setPokemon] = useState([])
   const [word, setWord] = useState('')
 
+//   useEffect(() => {
+//     axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${props.num}`)
+//         .then((res) => {
+//             console.log(res.data)
+//             setPokemon(res.data.results)
+//         })
+//   }, [props.num])
+
   useEffect(() => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${props.num}`)
-        .then((res) => {
-            console.log(res.data)
-            setPokemon(res.data.results)
-        })
-  }, [props.num])
+    const getPokemon = async () => {
+        let arrayOfPokemon = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon?limit=${props.num}`
+            );
+
+        setPokemon(arrayOfPokemon.data.results);
+        };
+
+        getPokemon();
+  }, []);
 
   const pokemonList = pokemon.map((poke) => 
   <li key={poke.name}>{poke.name}</li>)
