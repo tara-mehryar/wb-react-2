f139597ecff094370081f9b28b2b4ddd4484d9bf
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-29 10:36:57 -0500

    Wk 4 Day 2 React 2 Demo

--- wb-react-2/src/MovieList.jsx
+++ wb-react-2/src/MovieList.jsx
@@ -1,10 +1,34 @@
 import { useState } from 'react';
 
-export default function MovieList() {
+const MovieList = () =>  {
   const [movies, setMovies] = useState(['Alien', 'Predator', 'Alien Vs. Predator']);
   // This initializes our state value movies as an array with 3 items.
 
   const [inputValue, setInputValue] = useState('');
   // This will be used to hold the value of our input box.
 
+  // console.log(inputValue)
+
+  const movieDisplay = movies.map((movie) => <h5 key={movie}>{movie}</h5>)
+
+
+  return(
+    <div>
+      <h2>Movie List</h2>
+
+
+      <input value={inputValue} onChange={(e)=> setInputValue(e.target.value) } type="text" />
+
+      <button onClick={() => {
+      let newMoviesArr = [...movies, inputValue]
+      setMovies[newMoviesArr]
+      }}>
+        Add Movie
+      </button>
+    </div>
+  )
+
+
 }
+
+export default MovieList
