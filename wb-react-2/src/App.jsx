9f83d3043d8d199975da5acc18bb8bf2530c7fb0
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-29 10:13:14 -0500

    Wk 4 Day 2 React Day 2 Demo

--- wb-react-2/src/App.jsx
+++ wb-react-2/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import Counters from './Counters.jsx';
-// import Die from './Die.jsx';
-// import Layout from './Layout.jsx';
+import Die from './Die.jsx';
+import Layout from './Layout.jsx';
 // import MissingKey from './MissingKey.jsx';
 // import MovieList from './MovieList.jsx';
 // import Pokemon from './Pokemon.jsx';
@@ -27,10 +27,10 @@ export default function App() {
               </div>
             }
           />
-          <Route path="/movies" element={<MovieList />} />
+          {/* <Route path="/movies" element={<MovieList />} />
           <Route path="/missing-key" element={<MissingKey />} />
           <Route path="/counters" element={<Counters />} />
-          <Route path="/pokemon" element={<Pokemon num={10} />} />
+          <Route path="/pokemon" element={<Pokemon num={10} />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
